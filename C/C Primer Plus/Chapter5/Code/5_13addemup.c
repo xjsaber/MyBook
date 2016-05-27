00d1d833087914d6ad9aa4d6d05ee29188a944f8
xjsaber <xjsaber@yeah.net>
2016-05-27 12:59:56 +0800

    c/c primer plus add 5_13addemup.c

---
+++ C/C Primer Plus/Chapter5/Code/5_13addemup.c
@@ -0,0 +1,14 @@
+/* addemup.c -- 几种常见的语句 */
+#include <stdio.h>
+
+int main(void) {
+    int count, sum;
+
+    count = 0;
+    sum = 0;
+    while (count++ < 20)
+        sum = sum + count;
+    printf("sun = %d\n", sum);
+
+    return 0;
+}
