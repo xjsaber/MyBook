b795370d3f13148663bcb6f364b372cf7fe06b15
xjsaber <xjsaber@yeah.net>
2016-06-29 23:35:38 +0800

    Java/Spring_In_Action4 第一章

---
+++ Java/Spring_In_Action4/Chapter1/Code/01_HelloWorldBean.java
@@ -0,0 +1,12 @@
+package com.Spring_In_Action4.spring;
+
+import java.lang.String;
+
+/**
+ * Created by xjsaber on 3/13/2016.
+ */
+public class HelloWorldBean  {
+    public String sayHello(){
+        return "Hello World";
+    }
+}
