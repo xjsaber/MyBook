72cb3ab92c53a3f5f0319463d1f31e4185ca5273
xjsaber <xjsaber@yeah.net>
2017-02-20 23:22:35 +0800

    add C++Programming-Program Design Include Data Structures\ chapter4

---
+++ Learn/DataStructure/C++Programming-Program Design Include Data Structures/Chapter4/code/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    bool found = true;
+    bool flag = false;
+    int num = 1;
+    double x = 5.2;
+    double y = 3.4;
+    int a = 5, b = 8;
+    int n = 20;
+    char ch = 'B';
+
+    std::cout << "Line 1: !found evaluates to "
+              << !found << std::endl;
+
+
+
+    return 0;
+}
