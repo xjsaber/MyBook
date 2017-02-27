0f3a58e437e41986e7812596af0d8feaf3201739
xjsaber <xjsaber@yeah.net>
2017-02-27 22:20:28 +0800

    add 算法4/ch1, ch2 and code example

---
+++ Learn/Algorithm/算法4/ch2/code/Example.java
@@ -0,0 +1,66 @@
+package code;
+
+/**
+ * Created by xjsaber on 2017/2/27.
+ *
+ */
+public class Example {
+    /**
+     *
+     * @param a
+     */
+    public static void sort(Comparable[] a){    }
+
+    /**
+     *
+     * @param v
+     * @param w
+     * @return
+     */
+    private static boolean less(Comparable<Comparable> v, Comparable w){
+        return v.compareTo(w) < 0;
+    }
+
+    /**
+     *
+     * @param a
+     * @param i
+     * @param j
+     */
+    private static void exch(Comparable[] a, int i, int j){
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    /**
+     *
+     * @param a
+     */
+    private static void show(Comparable[] a){
+        for (int i = 0; i < a.length; i++){
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    /**
+     *
+     * @param a
+     * @return
+     */
+    public static boolean isSorted(Comparable[] a){
+        for (int i = 0; i < a.length; i++){
+            if (less(a[i], a[i-1])) return false;
+        }
+        return true;
+    }
+
+//    public static void main(String[] args){
+//        Scanner s = new Scanner(System.in);
+//        String[] a = s.next().split('');
+//        sort(a);
+//        assert isSorted(a);
+//        show(a);
+//    }
+}
