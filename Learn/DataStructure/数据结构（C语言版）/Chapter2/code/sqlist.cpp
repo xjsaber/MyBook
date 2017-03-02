c905260488742b8732bce1e39b25d80c7d5b0753
xjsaber <xjsaber@yeah.net>
2017-03-02 23:03:06 +0800

    init 数据结构（C语言版） 线性表 sqlist

---
+++ Learn/DataStructure/数据结构（C语言版）/Chapter2/code/sqlist.cpp
@@ -0,0 +1,32 @@
+//
+// Created by xjsaber on 2017/3/1.
+//
+#include "sqlist.h"
+
+Status InitList_Sq(SqList &L){
+
+}
+
+Status SqList_Class::LocateElem(SqList L, ElemType e){
+    int index = 0;
+    for (int i = 0; i < L.length; i++){
+        if (e == L.elem[i]){
+            index = i;
+            break;
+        }
+    }
+    return index;
+}
+
+Status SqList_Class::Insert(SqList &L, int i, ElemType e){
+    int index;
+    if (i < 1 || i > L.length + 1 || L.length){
+        return ERROR;
+    }
+    for (index = L.length; index >= i; --index){
+        L.elem[index+1] = L.elem[index];
+        L.elem[i] = e;
+        ++(L.length);
+        return OK;
+    }
+}
