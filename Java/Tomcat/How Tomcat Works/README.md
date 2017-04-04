5e00296cf1e98850b478a3c844c53d578a9d9c0f
xjsaber <xjsaber@yeah.net>
2017-04-04 23:28:59 +0800

    add Java/Tomcat/How Tomcat Works/ ch1 and code

---
+++ Java/Tomcat/How Tomcat Works/README.md
@@ -0,0 +1,6 @@
+# How Tomcat Works中文版 #
+
+第一章：一个简单的Web服务器
+
+
+
---
+++ Java/Tomcat/How Tomcat Works/ch1/code/SocketCode.java
@@ -0,0 +1,41 @@
+package com.xjsaber.socket;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * Created by xjsaber on 2017/4/4.
+ *
+ */
+public class SocketUtils {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Socket socket = new Socket("127.0.0.1", 8080);
+        OutputStream os = socket.getOutputStream();
+        PrintWriter out = new PrintWriter(
+                os, true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        // send an HTTP request to the web server
+        out.println("GET /index.jsp HTTP/1.1");
+        out.println("Host: localhost:8080");
+        out.println("Connection: Close");
+        out.println();
+        // read the response
+        Boolean loop = true;
+        StringBuilder sb = new StringBuilder();
+        while (loop){
+            if (in.ready()){
+                int i = 0;
+                while (i != -1){
+                    i = in.read();
+                    sb.append((char)i);
+                }
+                loop = false;
+            }
+            Thread.sleep(50);
+        }
+        //display the response to the out console
+        System.out.println(sb.toString());
+        socket.close();
+    }
+}
