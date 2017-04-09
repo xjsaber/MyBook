d1a18be28b0d058abfbb8facc15d59ab6b83d8e1
xjsaber <xjsaber@yeah.net>
2017-04-09 23:37:17 +0800

    add Netty权威指南 ch2 code nio

---
+++ Java/Netty/Netty权威指南/ch2/code/nio/MultiplexerTimeServer.java
@@ -0,0 +1,125 @@
+package com.xjsaber.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by xjsaber on 2017/4/9.
+ *
+ */
+public class MultiplexerTimeServer implements Runnable{
+
+    private Selector selector;
+
+    private ServerSocketChannel servChannel;
+
+    private volatile boolean stop;
+
+    /**
+     * 初始化多路复用器、绑定监听端口
+     * @param port
+     */
+    public MultiplexerTimeServer(int port){
+        try {
+            selector = Selector.open();
+            servChannel = ServerSocketChannel.open();
+            servChannel.configureBlocking(false);
+            servChannel.socket().bind(new InetSocketAddress(port), 1024);
+            servChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("The time server is start in port: " + port);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public void stop(){
+        this.stop = stop;
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void run() {
+        while (!stop){
+            try {
+                selector.select(1000);
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> it = selectionKeys.iterator();
+                SelectionKey key = null;
+                while (it.hasNext()) {
+                    key = it.next();
+                    it.remove();
+                    try {
+
+                    }
+                    catch (Exception e){
+                        if (key != null) {
+                            key.cancel();
+                            if (key.channel() != null) {
+                                key.channel().close();
+                            }
+                        }
+                    }
+                }
+            }catch (Throwable t){
+                t.printStackTrace();
+            }
+        }
+    }
+
+    private void handleInput(SelectionKey key) throws IOException {
+        if (key.isValid()) {
+            // 处理新接入的请求消息
+            if (key.isAcceptable()) {
+                // Accept the new connection
+                ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
+                SocketChannel sc = ssc.accept();
+                sc.configureBlocking(false);
+                // Add the new connection to the selector
+                sc.register(selector, SelectionKey.OP_READ);
+            }
+            if (key.isReadable()) {
+                // Read the data
+                SocketChannel sc = (SocketChannel) key.channel();
+                ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                int readBytes = sc.read(readBuffer);
+                if (readBytes > 0) {
+                    readBuffer.flip();
+                    byte[] bytes = new byte[readBuffer.remaining()];
+                    readBuffer.get(bytes);
+                    String body = new String(bytes, "UTF-8");
+                    System.out.println("The time server receive order : " + body);
+                    String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+
+                } else if (readBytes < 0) {
+                    // 对端链路关闭
+                    key.cancel();
+                    sc.close();
+                } else {
+                    ; //读到0字节， 忽略
+                }
+            }
+        }
+    }
+
+    private void doWrite(SocketChannel channel, String response) throws IOException{
+        if (response != null && response.trim().length() > 0) {
+            byte[] bytes = response.getBytes();
+            ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
+            writeBuffer.put(bytes);
+            writeBuffer.flip();
+            channel.write(writeBuffer);
+        }
+    }
+}
