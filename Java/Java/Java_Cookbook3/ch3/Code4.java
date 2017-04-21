280df5c90a3715699a7baf465b2ce1731cc97e74
xjsaber <xjsaber@yeah.net>
2017-04-21 17:37:43 +0800

    add Java/Java/Java_Cookbook3/ch3/

---
+++ Java/Java/Java_Cookbook3/ch3/Code4.java
@@ -0,0 +1,28 @@
+package com.yunrer.test;
+
+/**
+ * Created by xjsaber on 2017/4/21.
+ *
+ */
+public class Code4 {
+
+    public static void main(String[] args){
+        // char的位置
+        /*
+        String a = "A quick bronze fox lept a lazy bovine";
+        for (int i = 0; i < a.length(); i++) {
+            System.out.println("Char " + i + " is " + a.charAt(i));
+        }
+        */
+
+        // 将str转换成char的数组
+        /*
+        String s = "Hello World";
+        for (char ch : s.toCharArray()){
+            System.out.println(ch);
+        }
+        */
+
+        // 统计输入流的数量
+    }
+}
