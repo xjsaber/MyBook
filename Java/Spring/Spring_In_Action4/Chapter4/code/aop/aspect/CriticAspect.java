9342e3368f36b5c58c1540a91747289e4d01b0df
xjsaber <xjsaber@yeah.net>
2017-04-23 11:47:01 +0800

    add Java/Spring/Spring_In_Action4/Chapter4/ 第四章 还缺aop的xml形式和aspectJ的扩展，先放放，之后回头读

---
+++ Java/Spring/Spring_In_Action4/Chapter4/code/aop/aspect/CriticAspect.java
@@ -0,0 +1,12 @@
+package com.xjsaber.spring.aop.aspect;
+
+/**
+ * Created by xjsaber on 2017/4/23.
+ *
+ */
+public class CriticAspect {
+
+//    public CriticAspect() {}
+
+//    pointcut performance() : ex
+}
