31d765f62a45599611c809d49ab9dab366fb8ccf
xjsaber <xjsaber@yeah.net>
2017-06-14 10:36:41 +0800

    finish Redis实战 code 8-3

--- Db/Redis/Redis实战/code/pRedis/ch8/test_ch8.py
+++ Db/Redis/Redis实战/code/pRedis/ch8/test_ch8.py
@@ -21,21 +21,21 @@ class TestCh8(TestCase):
     def test_get_status_messages(self):
         conn = redis.Redis(host="localhost", port='6379', db=7)
         uid = '139960061'
-        get_status_messages(conn, uid)
+        print(get_status_messages(conn, uid))
         self.assertTrue("123")
 
+    # 主页面内容
+    def test_set_timeline(self):
+        conn = redis.Redis(host="localhost", port='6379', db=7)
+        timeline = 'home'
+        id = 139960061
+        conn.zadd('%s:%s' % (timeline, id), '227138379358277633', time.time())
+        conn.zadd('%s:%s' % (timeline, id), '227140001668935680', time.time())
+        conn.zadd('%s:%s' % (timeline, id), '227143088878014464', time.time())
 
 if __name__ == '__main__':
     t = TestCh8()
     # t.test_create_user()
     # t.test_create_status()
+    # t.test_set_timeline()
     t.test_get_status_messages()
-    #
-    # conn = redis.Redis(host="localhost", port='6379', db=7)
-    # timeline = 'home'
-    # id = 139960061
-    # conn.hmset('%s%s' % (timeline, id), {
-    #     '227138379358277633': time.time(),
-    #     '227140001668935680': time.time(),
-    #     '227143088878014464': time.time(),
-    # })
