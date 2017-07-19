ff2abe46ea97d6b65febe635fdb2df7dac5c80fd
xjsaber <xjsaber@yeah.net>
2017-07-19 17:22:42 +0800

    add Netty_in_action MyChannel

--- Java/Netty/Netty_In_Action/code/src/main/java/channel/MyChannel.java
+++ Java/Netty/Netty_In_Action/code/src/main/java/channel/MyChannel.java
@@ -1,22 +1,68 @@
 package channel;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * Created by xjsaber on 2017/7/17.
  *
  */
 public class MyChannel {
 
-    public static void main(String[] args) {
-//        EventLoopGroup group = new NioEventLoopGroup();
-//        try {
-//            ServerBootstrap b = new ServerBootstrap();
-//            b.group(group).channel(NioServerSocketChannel.class);
-//
-//            Channel channel = b.;
-//            ByteBuf buf = Unpooled.copiedBuffer("your data", Charset.forName("UTF-8"));
-//            ChannelFuture cf = channel
-//        } throw Exception{
-//
-//        }
+    public void server(int port) throws IOException {
+
+        ServerSocketChannel serverChannel = ServerSocketChannel.open();
+        serverChannel.configureBlocking(false);
+        ServerSocket ssocket = serverChannel.socket();
+        InetSocketAddress address = new InetSocketAddress(port);
+        ssocket.bind(address);
+        Selector selector = Selector.open();
+        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+        final ByteBuffer msg = ByteBuffer.wrap("Hi\r\n".getBytes());
+        for (;;) {
+            try {
+                selector.select();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                break;
+            }
+            Set<SelectionKey> readyKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = readyKeys.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                iterator.remove();
+                try {
+                    if (key.isAcceptable()) {
+                        ServerSocketChannel server = (ServerSocketChannel)key.channel();
+                        java.nio.channels.SocketChannel client = server.accept();
+                        client.configureBlocking(false);
+                        client.register(selector, SelectionKey.OP_WRITE | SelectionKey.OP_READ, msg.duplicate());
+
+                        if (key.isWritable()) {
+                            client = (java.nio.channels.SocketChannel) key.channel();
+                            ByteBuffer buffer = (ByteBuffer)key.attachment();
+                            while (buffer.hasRemaining()) {
+                                if (client.write(buffer) == 0) {
+                                    break;
+                                }
+                            }
+                            client.close();
+                        }
+                    }
+                } catch (IOException ex) {
+                    key.cancel();
+                    try {
+                        key.channel().close();
+                    } catch (IOException ignored) {}
+                }
+            }
+        }
     }
 }
