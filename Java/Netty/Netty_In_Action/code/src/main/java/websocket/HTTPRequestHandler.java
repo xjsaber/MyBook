6f3accc852367e8bb938869567c6818f4a36a848
xjsaber <xjsaber@yeah.net>
2017-07-24 17:44:37 +0800

    init Netty in action ch12

---
+++ Java/Netty/Netty_In_Action/code/src/main/java/websocket/HTTPRequestHandler.java
@@ -0,0 +1,41 @@
+package websocket;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.FullHttpRequest;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+/**
+ * 扩展SimpleChannelInboundHandler以处理FullHttpRequest消息
+ */
+public class HTTPRequestHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+
+    private final String wsUri;
+    private static final File INDEX;
+
+    public HTTPRequestHandler(String wsUri) {
+        this.wsUri = wsUri;
+    }
+
+    static {
+        URL location = HTTPRequestHandler.class
+                .getProtectionDomain()
+                .getCodeSource().getLocation();
+
+        try {
+            String path = location.toURI() + "index.html";
+            path = !path.contains("file:") ? path : path.substring(5);
+            INDEX = new File(path);
+        } catch (URISyntaxException e) {
+            throw new IllegalStateException("Unable to locate index.html", e);
+        }
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, FullHttpRequest fullHttpRequest) throws Exception {
+
+    }
+}
