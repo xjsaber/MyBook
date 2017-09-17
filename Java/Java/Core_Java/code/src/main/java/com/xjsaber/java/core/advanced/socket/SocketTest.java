9c065d6e20912f09f4775f09fb750ad3659f7fdd
xjsaber <xjsaber@yeah.net>
2017-09-17 21:48:36 +0800

    初始化 coreJava ch1 and ch2 ch3

---
+++ Java/Java/Core_Java/code/src/main/java/com/xjsaber/java/core/advanced/socket/SocketTest.java
@@ -0,0 +1,24 @@
+package com.xjsaber.java.core.advanced.socket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SocketTest {
+
+    public static void main(String[] args){
+        try (Socket socket = new Socket("www.baidu.com", 13)){
+            InputStream inputStream = socket.getInputStream();
+            Scanner in = new Scanner(inputStream);
+
+            while (in.hasNext()){
+                String line = in.nextLine();
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
