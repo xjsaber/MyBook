696df4aeafbe2ee4a41cea63d98725cbb52a23dd
xjsaber <xjsaber@yeah.net>
2018-06-19 08:25:10 +0800

    add docs.microsoft.com/code/CovarianceAndContravariance.cs

---
+++ Dot.NET/C_Sharp/docs.microsoft.com/code/code/CovarianceAndContravariance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace code
+{
+    class CovarianceAndContravariance
+    {
+        static object GetObject() { return null; }
+        static void SetObject(object obj) { }
+
+        static string GetString() { return ""; }
+        static void SetString(string str) { }
+
+        public void Test() {
+
+            IEnumerable<string> strings = new List<string>();
+            IEnumerable<object> objects = strings;
+
+            Action<object> actObject = SetObject;
+            Action<string> actString = actObject;
+
+            Func<object> del = GetString;
+
+            Action<string> del2 = SetObject;
+        }
+    }
+}
---
+++ Dot.NET/C_Sharp/docs.microsoft.com/code/code/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //const int One = 0b0001;
+            //const int Two = 0b0010;
+            //const int Four = 0b0100;
+            //const int Eight = 0b1000;
+
+            //string str = "test";
+            //object obj = str;
+
+            //Console.ReadKey();
+
+        }
+    }
+}
