ac3734cc532ecebce0289ebbf12aa827af86581c
xjsaber <xjsaber@yeah.net>
2018-06-19 23:09:29 +0800

    add docs.microsoft.com/code/ ch6/Expression-bodied.cs

--- Dot.NET/C_Sharp/docs.microsoft.com/code/code/Program.cs
+++ Dot.NET/C_Sharp/docs.microsoft.com/code/code/Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ExtensionMethodsExample;
+using System;
 
 namespace code
 {
@@ -7,6 +7,20 @@ namespace code
     {
         static void Main(string[] args)
         {
+            // t1 int
+            // t2 int 
+            // tResult int
+            //public delegate TResult Func<T1, T2, TResult>(T1 arg1, T2 arg2);
+            //Func<int, int, int> function = (a, b) => a + b;
+
+            //int c = function(3, 5);
+
+            //Console.WriteLine(c);
+            //Console.ReadKey();
+
+            //string temp = "123";
+            //temp.WordCount();
+
             //const int One = 0b0001;
             //const int Two = 0b0010;
             //const int Four = 0b0100;
@@ -16,7 +30,20 @@ namespace code
             //object obj = str;
 
             //Console.ReadKey();
+            Program p = new Program();
+            Console.WriteLine(p.ToString());
+            Console.WriteLine(p.FullName);
+            Console.ReadKey();
 
+        }
+
+        
+
+        static int Give()
+        {
+            string temp = "12";
+            temp.WordCount();
+            return 10;
         }
     }
 }
