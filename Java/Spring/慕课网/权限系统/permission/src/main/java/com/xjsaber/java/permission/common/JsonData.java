def756f0f96576a0ed0a1ba7dc7dd6328db9c139
xjsaber <xjsaber@yeah.net>
2018-11-01 11:13:51 +0800

    权限系统/添加json返回值公共类

---
+++ Java/Spring/慕课网/权限系统/permission/src/main/java/com/xjsaber/java/permission/common/JsonData.java
@@ -0,0 +1,47 @@
+package com.xjsaber.java.permission.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JsonData {
+
+    /**
+     * 表示接口数据状态，true or false
+     * true 从data里面拿数据
+     * false 表示数据返回不回来，从msg里拿数据信息
+     */
+    private boolean ret;
+
+    private String msg;
+
+    private Object data;
+
+    public JsonData(boolean ret){
+        this.ret = ret;
+    }
+
+    public static JsonData success(Object object, String msg){
+        JsonData jsonData = new JsonData(true);
+        jsonData.data = object;
+        jsonData.msg = msg;
+        return jsonData;
+    }
+
+    public static JsonData success(Object object){
+        JsonData jsonData = new JsonData(true);
+        jsonData.data = object;
+        return jsonData;
+    }
+
+    public static JsonData jsonData(){
+        return new JsonData(true);
+    }
+
+    public static JsonData fail(String msg){
+        JsonData jsonData = new JsonData(true);
+        jsonData.msg = msg;
+        return jsonData;
+    }
+}
