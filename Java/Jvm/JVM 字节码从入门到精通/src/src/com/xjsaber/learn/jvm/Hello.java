fa3eaa0f1997e73882c3e2fa6f2f5606dde51280
xjsaber <xjsaber@yeah.net>
2019-06-25 21:23:53 +0800

    init go/go实战

--- Java/Jvm/JVM 字节码从入门到精通/src/src/com/xjsaber/learn/jvm/Hello.java
+++ Java/Jvm/JVM 字节码从入门到精通/src/src/com/xjsaber/learn/jvm/Hello.java
@@ -1,3 +1,5 @@
+package com.xjsaber.learn.jvm;
+
 public class Hello {
     
     public static void main(String[] args) {
--- Java/Jvm/JVM 字节码从入门到精通/src/src/com/xjsaber/learn/jvm/Main.java
+++ Java/Jvm/JVM 字节码从入门到精通/src/src/com/xjsaber/learn/jvm/Main.java
@@ -1,3 +1,5 @@
+package com.xjsaber.learn.jvm;
+
 public class Main {
 
     public static void main(String[] args) {
