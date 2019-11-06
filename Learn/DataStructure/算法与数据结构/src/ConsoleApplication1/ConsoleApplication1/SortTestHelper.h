4cef844b2425f986952570f561089f652beecc8a
xjsaber <xjsaber@yeah.net>
2019-11-06 09:08:11 +0800

    sync

--- Learn/DataStructure/算法与数据结构/src/ConsoleApplication1/ConsoleApplication1/SortTestHelper.h
+++ Learn/DataStructure/算法与数据结构/src/ConsoleApplication1/ConsoleApplication1/SortTestHelper.h
@@ -1,7 +1,10 @@
 #pragma once
 
-#ifdef SELECTIONSORT_SORTTESTHELPER_H
 #define SELECTIONSORT_SORTTESTHELPER_H
+
+#include <iostream>
+#include <time.h>
+#include <cassert>
 
 using namespace std;
 
@@ -9,8 +12,13 @@ namespace SortTestHelper {
 
 	// 生成有n个元素的随机数组，每个元素的随机范围为[rangeL, rangeR]
 	int* generateRandomArray(int n, int rangeL, int rangeR) {
+		int* arr = new int[n];
 
+		assert(rangeL <= rangeR);
+		srand(time(nullptr));
+        for (int i = 0; i < n; i++){
+            arr[i] = rand() % (rangeR - rangeL + 1) + rangeL;
+        }
+		return arr;
 	}
 }
-
-#endif // SELECTIONSORT_SORTTESTHELPER_H
