7d1dd8aeee24e553a6569495a6344691fc9465af
xjsaber <xjsaber@yeah.net>
2019-11-17 23:04:28 +0800

    TODO 算法图解和深入计算机操作系统

---
+++ Java/Java/Java并发编程实战/src/src/com/xjsaber/learn/java/Account.java
@@ -0,0 +1,20 @@
+package com.xjsaber.learn.jvm;
+
+/**
+ * 假设线程 T1 执行账户 A 转账户 B 的操作，账户 A.transfer(账户 B)；同时线程 T2 执行账户 B 转账户 A 的操作，账户 B.transfer(账户 A)。当 T1 和 T2 同时执行完①处的代码时，T1 获得了账户 A 的锁（对于 T1，this 是账户 A），而 T2 获得了账户 B 的锁（对于 T2，this 是账户 B）。之后 T1 和 T2 在执行②处的代码时，T1 试图获取账户 B 的锁时，发现账户 B 已经被锁定（被 T2 锁定），所以 T1 开始等待；T2 则试图获取账户 A 的锁时，发现账户 A 已经被锁定（被 T1 锁定），所以 T2 也开始等待。于是 T1 和 T2 会无期限地等待下去，也就是我们所说的死锁了。
+ * @author xjsaber
+ */
+public class Account {
+    private int balance;
+    void transfer(Account target, int amt){
+        synchronized (this){
+            synchronized (target){
+                if (this.balance > amt){
+                    this.balance -= amt;
+                    target.balance += amt;
+                }
+            }
+        }
+    }
+
+}
