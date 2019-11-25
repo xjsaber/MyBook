8b530b7c10d5cbd507867bd6cd5949d9da62c6ee
xjsaber <xjsaber@yeah.net>
2019-11-25 14:05:09 +0800

    init Hash内容

---
+++ Learn/DataStructure/Data Structures and Algorithm Analysis in C/src/include/D_Hash_H.h
@@ -0,0 +1,24 @@
+//
+// Created by xjsaber on 2019/11/19.
+//
+
+#ifndef DATA_STRUCTURES_AND_ALGORITHM_ANALYSIS_IN_C_D_STACK_H_H
+#define DATA_STRUCTURES_AND_ALGORITHM_ANALYSIS_IN_C_D_STACK_H_H
+
+struct Node;
+typedef struct Node *PtrToNode;
+typedef unsigned int Index;
+typedef PtrToNode Stack;
+typedef int ElementType;
+
+Index Hash(const char* key, int tableSize);
+
+int IsEmpty(Stack s);
+Stack CreateStack(void);
+void DisposeStack(Stack s);
+void MakeEmpty(Stack s);
+void Push(ElementType x, Stack s);
+ElementType Top(Stack s);
+void Pop(Stack s);
+
+#endif //DATA_STRUCTURES_AND_ALGORITHM_ANALYSIS_IN_C_D_LIST_H_H
