000e6dca9f30135836d355c0061d49be1c95b97e
xjsaber <xjsaber@yeah.net>
2020-01-12 09:05:57 +0800

    add 第9讲 | 对比Hashtable、HashMap、TreeMap有什么不同

---
+++ Java/Java/Java核心技术面试精讲/src/core/src/main/java/com/xjsaber/learn/java/mian/core/concurrent/MyHashTable.java
@@ -0,0 +1,11 @@
+package com.xjsaber.learn.java.mian.core.concurrent;
+
+import java.util.Hashtable;
+
+/**
+ * @author xjsaber
+ */
+public class MyHashTable {
+
+    Hashtable<String, String> myHashTable = new Hashtable<>();
+}
---
+++ Java/Java/Java核心技术面试精讲/src/core/src/main/java/com/xjsaber/learn/java/mian/core/map/MyLinkedHashMap.java
@@ -0,0 +1,12 @@
+package com.xjsaber.learn.java.mian.core.map;
+
+import java.util.LinkedHashMap;
+
+/**
+ * @author xjsaber
+ * @see LinkedHashMap
+ */
+public class MyLinkedHashMap {
+
+
+}
