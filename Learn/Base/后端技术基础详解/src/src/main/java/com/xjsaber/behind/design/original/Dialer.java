bf5023b684e1eaa973c38e042ee7f9c60f8e0051
xjsaber <xjsaber@yeah.net>
2020-04-03 17:50:38 +0800

    sync

---
+++ Learn/Base/后端技术基础详解/src/src/main/java/com/xjsaber/behind/design/original/Dialer.java
@@ -0,0 +1,12 @@
+package com.xjsaber.behind.design.original;
+
+public class Dialer {
+
+    public void enterDigit(int digit) {
+        System.out.println("enter digit: " + digit);
+    }
+
+    public void dial() {
+        System.out.println("dialing...");
+    }
+}
