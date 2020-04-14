72829c8812724371a12d74b9c0b76a7a30002f3c
xjsaber <xjsaber@yeah.net>
2020-04-14 17:44:46 +0800

    add 数据结构与算法之美ch6

--- Learn/DataStructure/数据结构与算法之美/src/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Learn/DataStructure/数据结构与算法之美/src/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,9 +3,57 @@
 
 #include <iostream>
 
+#define maxSize 100
+
+typedef struct SqlList
+{
+    int data[maxSize];
+    int length;
+} SqlList;
+
+typedef struct LNode
+{
+    int data;
+    struct LNode* next;
+}LNode;
+
+typedef struct DLNode
+{
+    int data;
+    struct DLNode* next;
+    struct DLNode* prev;
+}DLNode;
+
+SqlList createList(int length) 
+{
+    struct SqlList l;
+
+    for (int i = 0; i < length; i++) {
+        l.data[i] = i;
+    }
+    l.length = length;
+    return l;
+}
+
+int findElem(SqlList l, int item) 
+{
+    int index = -1;
+    for (int i = 0; i < l.length; i++) {
+        if (item == l.data[i]) {
+            // 序号是从1开始，i是从0开始
+            index = i + 1;
+            break;
+        }
+    }
+    return index;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int length = 50;
+    SqlList l = createList(50);
+    int index = findElem(l, 0);
+    std::cout << index;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
