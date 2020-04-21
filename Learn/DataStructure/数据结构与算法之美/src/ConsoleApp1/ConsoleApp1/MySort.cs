acc48bdab55c5ad17099b10dc4cadc70c8ced528
xjsaber <xjsaber@yeah.net>
2020-04-21 19:00:18 +0800

    sync

--- Learn/DataStructure/数据结构与算法之美/src/ConsoleApp1/ConsoleApp1/MySort.cs
+++ Learn/DataStructure/数据结构与算法之美/src/ConsoleApp1/ConsoleApp1/MySort.cs
@@ -30,6 +30,42 @@ namespace ConsoleApp1
             }
         }
 
+        /// <summary>
+        /// 快速排序
+        /// </summary>
+        /// <param name="a">array</param>
+        /// <param name="n">length</param>
+        public void QuickSort(int[] a, int n)
+        {
+            if (n <= 1) return;
+
+            for (int i = 0; i < n; i++)
+            {
+                int tmp = a[i];
+                for (int j = n - 1; j > 1; j++)
+                {
+                    if (a[j] > tmp)
+                    {
+                        MySwitch(ref tmp, ref a[j]);
+                    }
+                }
+            }
+            
+            bool flag = false;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        MySwitch(ref a[j], ref a[j + 1]);
+                        flag = true;
+                    }
+                }
+                if (!flag) break;
+            }
+        }
+
         private void MySwitch(ref int a, ref int b)
         {
             int temp = a;
--- Learn/DataStructure/数据结构与算法之美/src/ConsoleApp1/ConsoleApp1/Program.cs
+++ Learn/DataStructure/数据结构与算法之美/src/ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,11 +7,22 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             MySort mySort = new MySort();
-            int[] array = { 3, 5, 4, 1, 2, 6 };
-            mySort.BubbleSort(array, 6);
+            //int[] array = new int[2000];
+            //Init(array);
+            int[] array = new int[] { 3, 5, 8, 11, 9, 7, 5, 2 };
+            mySort.BubbleSort(array, 2000);
             foreach (int item in array)
             {
                 Console.WriteLine(item);
+            }
+        }
+
+        static void Init(int[] arr)
+        {
+            Random random = new Random();
+            for (int i = 0; i < 2000; i++)
+            {
+                arr[i] = random.Next(0, 9);
             }
         }
     }
