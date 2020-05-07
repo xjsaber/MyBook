4e278ea6b4e1de2dd0da5f1cbcaef78126734017
xjsaber <xjsaber@yeah.net>
2020-05-07 13:59:32 +0800

    add 数据结构与算法之美 35

---
+++ Learn/DataStructure/数据结构与算法之美/src/idea/src/main/java/com/xjsaber/learn/idea/search/trie/TrieTree.java
@@ -0,0 +1,36 @@
+package com.xjsaber.learn.idea.search.trie;
+
+import javax.swing.tree.TreeNode;
+
+public class TrieTree {
+
+    private TrieNode root = new TrieNode('/');
+
+    // 往Trie树中插入一个字符串
+    public void insert(char[] text) {
+        TrieNode p = root;
+        for (char c : text) {
+            int index = c - 'a';
+            if (p.children[index] == null) {
+                TrieNode newNode = new TrieNode(c);
+                p.children[index] = newNode;
+            }
+            p = p.children[index];
+        }
+        p.isEndingChar = true;
+    }
+
+    // 在Trie树中查找一个字符串
+    public boolean find(char[] pattern) {
+        TrieNode p = root;
+        for (int i = 0; i < pattern.length; i++) {
+            int index = pattern[i] - 'a';
+            if (p.children[index] == null){
+                return false;
+            }
+            p = p.children[index];
+        }
+        if (!p.isEndingChar) return false;
+        else return true; // 找到pattern
+    }
+}
