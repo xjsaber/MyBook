33f03be9304b54ad0f6dff2595ba64260e6e1a78
xjsaber <xjsaber@yeah.net>
2020-05-15 14:30:46 +0800

    add 数据结构与算法之美  ch17

---
+++ Learn/DataStructure/数据结构与算法之美/src/idea/src/main/java/com/xjsaber/learn/idea/algorithm/MyBag.java
@@ -0,0 +1,25 @@
+package com.xjsaber.learn.idea.algorithm;
+
+/**
+ * 0-1背包问题升级版
+ * @author xjsaber
+ */
+public class MyBag {
+
+    private int maxV = Integer.MIN_VALUE;
+    private int[] items = {2, 2, 4, 6, 3};
+    private int[] value = {3, 4, 8, 9, 6};
+    private int n = 5;
+    private int w = 9;
+
+    public void f(int i, int cw, int cv) {
+        if (cw == w || i == n){
+            if(cv > maxV) {
+                maxV = cv;
+                return;
+            }
+        }
+        f(i+1, cw, cv);
+        if (cw)
+    })
+}
