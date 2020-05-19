cc7348f0534587dd256abe40ef3bc8206fae0f96
xjsaber <xjsaber@yeah.net>
2020-05-19 15:58:57 +0800

    add 深入浅出计算机组成原理 ch5和ch6

---
+++ Learn/Microcomputer/深入浅出计算机组成原理/src/for.c
@@ -0,0 +1,12 @@
+//for
+
+#include <time.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 0;
+    for (int i = 0; i < 3; i++){
+        a += i;
+    }
+}
---
+++ Learn/Microcomputer/深入浅出计算机组成原理/src/ifelse.c
@@ -0,0 +1,19 @@
+//test.c
+
+#include <time.h>
+#include <stdlib.h>
+
+int main()
+{
+    srand(time(NULL));
+    int r = rand() % 2;
+    int a = 10;
+    if (r == 0)
+    {
+        a = 1;
+    }
+    else 
+    {
+        a = 2;
+    }
+}
