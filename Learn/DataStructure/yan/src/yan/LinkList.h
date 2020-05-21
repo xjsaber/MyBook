2d32a4db4a99883b507ee382fe8af28cfd626261
xjsaber <xjsaber@yeah.net>
2020-05-21 18:18:46 +0800

    sync

--- Learn/DataStructure/yan/src/yan/LinkList.h
+++ Learn/DataStructure/yan/src/yan/LinkList.h
@@ -1,8 +1,15 @@
+#include "../Base.h"
 //
 // Created by xjsaber on 2020/5/21.
 //
 
 #ifndef SRC_LINKLIST_H
 #define SRC_LINKLIST_H
+
+typedef struct LNode
+{
+    ElemType data;
+    struct LNode *next;
+}LNode, *LinkList;
 
 #endif //SRC_LINKLIST_H
