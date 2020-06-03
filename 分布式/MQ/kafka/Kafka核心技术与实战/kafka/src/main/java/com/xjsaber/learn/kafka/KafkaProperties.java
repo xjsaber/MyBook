d048d1ea32c7e9cd517f740fa6b215127c245973
xjsaber <xjsaber@yeah.net>
2020-06-03 16:19:30 +0800

    add 左耳听风 ch21 | 分布式系统架构的冰与火

---
+++ 分布式/MQ/kafka/Kafka核心技术与实战/kafka/src/main/java/com/xjsaber/learn/kafka/KafkaProperties.java
@@ -0,0 +1,13 @@
+package com.xjsaber.learn.kafka;
+
+/**
+ * @author xjsaber
+ */
+public class KafkaProperties {
+    
+    public static final String TOPIC = "topic1";
+    public static final String KAFKA_SERVER_URL = "120.55.56.16";
+    public static final int KAFKA_SERVER_PORT = 9092;
+
+    private KafkaProperties() {}
+}
