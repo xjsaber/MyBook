e1515036ff7f807fe542508b8757429f5748f63c
xjsaber <xjsaber@yeah.net>
2020-06-11 23:39:48 +0800

    sync

--- Java/Spring/玩转Spring全家桶/src/spring/src/main/java/com/xjsaber/learn/spring/Application.java
+++ Java/Spring/玩转Spring全家桶/src/spring/src/main/java/com/xjsaber/learn/spring/Application.java
@@ -3,10 +3,12 @@ package com.xjsaber.learn.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author xjsaber
  */
+@RestController
 @SpringBootApplication
 public class Application {
 
