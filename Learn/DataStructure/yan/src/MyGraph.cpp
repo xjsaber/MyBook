b271e658d74114a5a89caa5f2394626592473067
xjsaber <xjsaber@yeah.net>
2020-07-24 11:08:11 +0800

    snyc

--- Learn/DataStructure/yan/src/MyGraph.cpp
+++ Learn/DataStructure/yan/src/MyGraph.cpp
@@ -1,4 +1,19 @@
 //
 // Created by xjsaber on 2020/7/23.
 //
+#include "MyGraph.h"
 
+int visit[MAXSIZE]
+
+void DFS(AGraph *G, int v)
+{
+    ArcNode *p;
+    visit[v] = 1;
+//    visit(v);
+
+}
+
+void visist(VertexType v)
+{
+
+}
--- Learn/DataStructure/yan/src/MyGraph.h
+++ Learn/DataStructure/yan/src/MyGraph.h
@@ -19,4 +19,23 @@ typedef struct
     VertexType vex[MAXSIZE];  // 存放结点信息
 }MGraph;	// 图的邻接矩阵类型
 
+typedef struct ArcNode
+{
+    int adjvex; // 这边所指向的结点的位置
+    struct ArcNode *nextarc; //指向下一条边的指针
+    int info;   // 该边的相关信息
+} ArcNode;
+
+typedef struct
+{
+    char data; //顶点信息
+    ArcNode *firstarc; //指向第一条边的指针
+} VNode;
+
+typedef struct
+{
+    VNode adjlist[MAXSIZE]; //邻接表
+    int n, e;   //顶点数和边数
+} AGraph;
+
 #endif //SRC_MYGRAPH_H
